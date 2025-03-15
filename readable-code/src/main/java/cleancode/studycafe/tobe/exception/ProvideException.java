4607c247ee97281bfa6e3b8605d8ce8cbd679321
discphy <dinophy@nate.com>
2025-03-15 11:38:24 +0900

    :rewind: 리뷰반영 ProvideException 커스텀 예외 AppException 하위 개념으로 재정의

--- readable-code/src/main/java/cleancode/studycafe/tobe/exception/ProvideException.java
+++ readable-code/src/main/java/cleancode/studycafe/tobe/exception/ProvideException.java
@@ -1,9 +1,19 @@
 package cleancode.studycafe.tobe.exception;
 
-public class ProvideException extends RuntimeException {
+import java.util.logging.Logger;
+
+public class ProvideException extends AppException {
+
+    private static final String PROVIDE_ERROR_MESSAGE = "주문 가능한 이용권이 없습니다.";
+    private static final Logger LOGGER = Logger.getLogger(ProvideException.class.getName());
 
     public ProvideException(String message) {
         super(message);
     }
 
+    @Override
+    public String getMessage() {
+        LOGGER.info("Provide Exception: " + super.getMessage());
+        return PROVIDE_ERROR_MESSAGE;
+    }
 }
